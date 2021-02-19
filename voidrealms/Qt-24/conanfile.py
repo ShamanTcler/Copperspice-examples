02991b7ce77913ca1f3b21ad8be1bf72d5647c7f
CandL <ce.slater@yahoo.com>
2021-02-19 09:07:37 -0500

    Working on a no package CS build of qt-24

--- voidrealms/Qt-24/conanfile.py
+++ voidrealms/Qt-24/conanfile.py
@@ -1,17 +1,34 @@
 import os
 import re
+import shutil
+import sys
 
 from fnmatch import fnmatch
 from conans import ConanFile, CMake, tools
 
-class CS_Download_Convert(ConanFile)
-    name = "Qt-24"
+class CS_Download_Convert(ConanFile):
+    name = os.path.basename(os.getcwd())    
+    print("Processing void realms project:",name)
+    print()
     version = "1.0"
-    description = "Voidrealms Qt-24 QSlider and the QProgressbar"
+    description = "Voidrealms:" + name
     license = "Creative Commons Attribution license (reuse allowed)"
     generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
 
-    def source(self)
-        tools.get("https://github.com/voidrealms/Qt-24/tree/7bd0a12387c4738acf481dbc7a72f9774e064c6e")
+    def source(self):
+        git_src="https://github.com/voidrealms/" + self.name
+        print("     Git_src:",git_src)
+
+        # Get the source from Github
+        git_org = tools.Git(folder="original_source")
+        git_org.clone(git_src, "master")
+        
+        # make a copy of it
+        shutil.copytree("original_source", "cs_source")
+
+        # run  PepperMill recursively on cs_source ( PepperMill --recursive <your_path> -f )
+        os.system("PepperMill --recursive cs_source -f")
+        
+        
 
